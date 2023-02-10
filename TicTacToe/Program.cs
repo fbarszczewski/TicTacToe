d432ea589e4606c2d24aed88e08db77a21cb7f7e
filip barszczewski <fbarszczewski@outlook.com>
2023-02-10 10:03:32 +0100

    Dodaj pliki projektów.

---
+++ TicTacToe/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe
+{
+    internal class Program
+    {
+        static char[,] playField =
+        {
+            {'1','2','3' },
+            {'4','5','6' },
+            {'7','8','9' }
+        };
+        static void Main(string[] args)
+        {
+            PrintBoard();
+            Console.Read();
+        }
+
+        public static void PrintBoard()
+        {
+            Console.WriteLine("     |     |     ");
+            Console.WriteLine("  {0}  |  {1}  |  {2}  ", 1,2,3);
+            Console.WriteLine("_____|_____|_____");
+            Console.WriteLine("     |     |     ");
+            Console.WriteLine("  {0}  |  {1}  |  {2}  ", 4, 5, 6);
+            Console.WriteLine("_____|_____|_____");
+            Console.WriteLine("     |     |     ");
+            Console.WriteLine("  {0}  |  {1}  |  {2}  ", 1, 2, 3);
+            Console.WriteLine("_____|_____|_____");
+            Console.WriteLine("     |     |     ");
+
+        }
+    }
+}
